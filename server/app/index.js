980b4abdbce00763b49fc39af4f768d056963a2d
kasiriveni <kasiriveni@gmail.com>
2019-07-04 09:24:05 +0530

    updated files

--- server/app/index.js
+++ server/app/index.js
@@ -1,3 +1,20 @@
-#!/usr/bin/env node
+const express = require("express");
 
-console.log(process)
+const app = express();
+console.log(app);
+
+app.get("/hello", (req, res) => {
+  res.send("hello world");
+});
+app.get("/login", (req, res) => {
+  // ?
+  res.send("helloss world");
+});
+app.get("/logout", (req, res) => {
+  res.send({
+    id: 1
+  });
+});
+app.listen(3000, () => {
+  console.log(`serve is running on`);
+});
